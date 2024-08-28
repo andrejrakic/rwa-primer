cc771b40a537f143536bf0d6e2334da5c2a85b05
Richard Gottleber <richard@gottleber.net>
2024-08-28 11:11:37 -0500

    add livingArea & bedroomsTotal

--- src/FunctionsSource.sol
+++ src/FunctionsSource.sol
@@ -7,20 +7,28 @@ pragma solidity ^0.8.0;
  * DO NOT USE THIS CODE IN PRODUCTION.
  */
 abstract contract FunctionsSource {
-    string public getNftMetadata = "const { ethers } = await import('npm:ethers@6.10.0');"
-        "const abiCoder = ethers.AbiCoder.defaultAbiCoder();" "const apiResponse = await Functions.makeHttpRequest({"
-        "    url: `https://api.bridgedataoutput.com/api/v2/OData/test/Property('P_5dba1fb94aa4055b9f29696f')?access_token=6baca547742c6f96a6ff71b138424f21`,"
-        "});" "const realEstateAddress = apiResponse.data.UnparsedAddress;"
-        "const yearBuilt = Number(apiResponse.data.YearBuilt);"
-        "const lotSizeSquareFeet = Number(apiResponse.data.LotSizeSquareFeet);"
-        "const encoded = abiCoder.encode([`string`, `uint256`, `uint256`], [realEstateAddress, yearBuilt, lotSizeSquareFeet]);"
-        "return ethers.getBytes(encoded);";
-
-    string public getPrices = "const { ethers } = await import('npm:ethers@6.10.0');"
-        "const abiCoder = ethers.AbiCoder.defaultAbiCoder();" "const tokenId = args[0];"
+    string public getNftMetadata =
+        "const { ethers } = await import('npm:ethers@6.10.0');"
+        "const abiCoder = ethers.AbiCoder.defaultAbiCoder();"
         "const apiResponse = await Functions.makeHttpRequest({"
         "    url: `https://api.bridgedataoutput.com/api/v2/OData/test/Property('P_5dba1fb94aa4055b9f29696f')?access_token=6baca547742c6f96a6ff71b138424f21`,"
-        "});" "const listPrice = Number(apiResponse.data.ListPrice);"
+        "});"
+        "const realEstateAddress = apiResponse.data.UnparsedAddress;"
+        "const yearBuilt = Number(apiResponse.data.YearBuilt);"
+        "const lotSizeSquareFeet = Number(apiResponse.data.LotSizeSquareFeet);"
+        "const livingArea = Number(apiResponse.data.LivingArea);"
+        "const bedroomsTotal = Number(apiResponse.data.BedroomsTotal);"
+        "const encoded = abiCoder.encode([`string`, `uint256`, `uint256`], [realEstateAddress, yearBuilt, lotSizeSquareFeet, livingArea, bedroomsTotal]);"
+        "return ethers.getBytes(encoded);";
+
+    string public getPrices =
+        "const { ethers } = await import('npm:ethers@6.10.0');"
+        "const abiCoder = ethers.AbiCoder.defaultAbiCoder();"
+        "const tokenId = args[0];"
+        "const apiResponse = await Functions.makeHttpRequest({"
+        "    url: `https://api.bridgedataoutput.com/api/v2/OData/test/Property('P_5dba1fb94aa4055b9f29696f')?access_token=6baca547742c6f96a6ff71b138424f21`,"
+        "});"
+        "const listPrice = Number(apiResponse.data.ListPrice);"
         "const originalListPrice = Number(apiResponse.data.OriginalListPrice);"
         "const taxAssessedValue = Number(apiResponse.data.TaxAssessedValue);"
         "const encoded = abiCoder.encode([`uint256`, `uint256`, `uint256`, `uint256`], [tokenId, listPrice, originalListPrice, taxAssessedValue]);"
